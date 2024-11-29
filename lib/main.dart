ec0c61817bc8f67c31f567ab0953d465dee4b6fc
Rouurouu <rayendjebbi2022@gmail.com>
2024-11-29 20:52:18 +0100

    use CircleAvatar ti set image

--- lib/main.dart
+++ lib/main.dart
@@ -11,16 +11,14 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
         home: Scaffold(
-      backgroundColor: Colors.red[700],
+      backgroundColor: Colors.cyan[700],
       body: SafeArea(
-        child: Container(
-          child: Text('Hello Rayen'),
-          color: Colors.white,
-          height: 200.0,
-          width: 200.0,
-          margin: EdgeInsets.only(left: 50.0),
-          padding: EdgeInsets.all(80.0),
-        ),
+        child: Column(children: [
+          CircleAvatar(
+            radius: 50.0,
+            backgroundImage: AssetImage('images/mine.jpg'),
+          ),
+        ]),
       ),
     ));
   }
